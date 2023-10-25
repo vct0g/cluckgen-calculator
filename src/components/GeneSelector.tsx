d3e2f511d88e482bfe3d76d182706f2f9adee522
vct0g <zzj@tutanota.com>
2023-10-25 18:19:44 -0400

    function update

--- src/components/GeneSelector.tsx
+++ src/components/GeneSelector.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 
-type GeneSelectorProps = {
+interface GeneSelectorProps {
   gene: string;
   alleles: string[];
-  onChange: (allele: string) => void;
-};
+  selectedAllele: string;
+  onSelectAllele: (allele: string) => void;
+}
 
 const GeneSelector: React.FC<GeneSelectorProps> = ({
   gene,
   alleles,
-  onChange,
+  selectedAllele,
+  onSelectAllele,
 }) => {
   return (
-    <div className="mb-4">
+    <div>
       <label htmlFor={gene} className="block text-sm font-medium text-gray-700">
         {gene}
       </label>
       <select
         id={gene}
-        onChange={(e) => onChange(e.target.value)}
-        className="p-2 border rounded mt-1"
+        value={selectedAllele}
+        onChange={(e) => onSelectAllele(e.target.value)}
+        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
       >
         {alleles.map((allele) => (
           <option key={allele} value={allele}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,10 @@ import Results from "../components/Results";
 const Home: React.FC = () => {
   const [geneData, setGeneData] = useState<any>({});
   const [selectedGene, setSelectedGene] = useState<string>("E"); // Default to 'E'
-  const [femaleGenes, setFemaleGenes] = useState<{ [key: string]: string }>({});
-  const [maleGenes, setMaleGenes] = useState<{ [key: string]: string }>({});
+  const [femaleGene, setFemaleGene] = useState<string>("E");
+  const [maleGene, setMaleGene] = useState<string>("E");
+  const [femaleAllele, setFemaleAllele] = useState<string>("E"); // Default to first allele of 'E'
+  const [maleAllele, setMaleAllele] = useState<string>("E"); // Default to first allele of 'E'
   const [results, setResults] = useState<string>("");
 
   useEffect(() => {
@@ -18,13 +20,11 @@ const Home: React.FC = () => {
         }
         const data = await response.json();
         setGeneData(data);
-        // Set default alleles for each gene
-        const defaultGenes: { [key: string]: string } = {};
-        Object.keys(data).forEach((gene) => {
-          defaultGenes[gene] = data[gene].alleles[0];
-        });
-        setFemaleGenes(defaultGenes);
-        setMaleGenes(defaultGenes);
+        const firstGene = Object.keys(data)[0];
+        setFemaleGene(firstGene);
+        setMaleGene(firstGene);
+        setFemaleAllele(data[firstGene]?.alleles[0]);
+        setMaleAllele(data[firstGene]?.alleles[0]);
       } catch (error) {
         console.error("ERROR fetching gene data", error);
       }
@@ -33,20 +33,21 @@ const Home: React.FC = () => {
   }, []);
 
   const handleFemaleGeneChange = (gene: string, allele: string) => {
-    setFemaleGenes({ ...femaleGenes, [gene]: allele });
+    setFemaleGene(gene);
+    setFemaleAllele(allele);
   };
 
   const handleMaleGeneChange = (gene: string, allele: string) => {
-    setMaleGenes({ ...maleGenes, [gene]: allele });
+    setMaleGene(gene);
+    setMaleAllele(allele);
   };
 
+  //core logic of calculation
   const calculateFeatherColor = (
     selectedGene: string,
-    femaleGenes: { [key: string]: string },
-    maleGenes: { [key: string]: string }
+    femaleAllele: string,
+    maleAllele: string
   ) => {
-    const femaleAllele = femaleGenes[selectedGene];
-    const maleAllele = maleGenes[selectedGene];
     const possibleCombinations = [
       femaleAllele + maleAllele,
       maleAllele + femaleAllele,
@@ -57,8 +58,8 @@ const Home: React.FC = () => {
   const calculateResults = () => {
     const featherColor = calculateFeatherColor(
       selectedGene,
-      femaleGenes,
-      maleGenes
+      femaleAllele,
+      maleAllele
     );
     setResults(`Potential Feather Color: ${featherColor}`);
   };
@@ -68,53 +69,90 @@ const Home: React.FC = () => {
       <h1 className="text-2xl font-bold mb-4">CluckGen Calculator</h1>
       <div className="mb-4">
         <label
-          htmlFor="selectedGene"
+          htmlFor="maleGene"
           className="block text-sm font-medium text-gray-700"
         >
-          Select Gene for Calculation:
+          Select Male Gene:
         </label>
         <select
-          id="selectedGene"
-          name="selectedGene"
-          value={selectedGene}
-          onChange={(e) => setSelectedGene(e.target.value)}
+          id="maleGene"
+          value={maleGene}
+          onChange={(e) => {
+            setMaleGene(e.target.value);
+            setMaleAllele(geneData[e.target.value]?.alleles[0]);
+          }}
           className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         >
-          {Object.keys(geneData).length > 0 ? (
-            Object.keys(geneData).map((gene) => (
-              <option key={gene} value={gene}>
-                {gene}
-              </option>
-            ))
-          ) : (
-            <option value="" disabled>
-              Loading...
+          {Object.keys(geneData).map((gene) => (
+            <option key={gene} value={gene}>
+              {gene}
             </option>
-          )}
+          ))}
         </select>
       </div>
-      <div>
-        <h2 className="text-xl font-bold mb-2">Female Genes</h2>
-        {Object.keys(geneData).map((gene) => (
-          <GeneSelector
-            key={`female-${gene}`}
-            gene={gene}
-            alleles={geneData[gene].alleles}
-            onChange={(allele) => handleFemaleGeneChange(gene, allele)}
-          />
-        ))}
-      </div>
 
-      <div>
-        <h2 className="text-xl font-bold mb-2 mt-4">Male Genes</h2>
-        {Object.keys(geneData).map((gene) => (
-          <GeneSelector
-            key={`male-${gene}`}
-            gene={gene}
-            alleles={geneData[gene].alleles}
-            onChange={(allele) => handleMaleGeneChange(gene, allele)}
-          />
-        ))}
+      <div className="mb-4">
+        <label
+          htmlFor="maleAllele"
+          className="block text-sm font-medium text-gray-700 mt-4"
+        >
+          Select Male Allele
+        </label>
+        <select
+          id="maleAllele"
+          value={maleAllele}
+          onChange={(e) => setMaleAllele(e.target.value)}
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        >
+          {geneData[maleGene]?.alleles.map((allele: string) => (
+            <option key={allele} value={allele}>
+              {allele}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="femaleGene"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Select Female Gene:
+        </label>
+        <select
+          id="femaleGene"
+          value={femaleGene}
+          onChange={(e) => {
+            setFemaleGene(e.target.value);
+            setFemaleAllele(geneData[e.target.value]?.alleles[0]); // Update female allele when gene changes
+          }}
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        >
+          {Object.keys(geneData).map((gene) => (
+            <option key={gene} value={gene}>
+              {gene}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="femaleAllele"
+          className="block text-sm font-medium text-gray-700 mt-4"
+        >
+          Select Female Allele:
+        </label>
+        <select
+          id="femaleAllele"
+          value={femaleAllele}
+          onChange={(e) => setFemaleAllele(e.target.value)}
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        >
+          {geneData[femaleGene]?.alleles.map((allele: string) => (
+            <option key={allele} value={allele}>
+              {allele}
+            </option>
+          ))}
+        </select>
       </div>
 
       <button onClick={calculateResults} className="p-2 border rounded mt-4">
