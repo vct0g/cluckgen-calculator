5f5c90431b3625a46494317e27e5cfd586f37ca1
vct0g <zzj@tutanota.com>
2023-10-26 12:26:32 -0400

    remove unused components and import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import GeneSelector from "../components/GeneSelector";
 import Results from "../components/Results";
 
 const Home: React.FC = () => {
