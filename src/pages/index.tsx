e41ff1d9c9b4482d3de6e0d5171b44b358658741
vct0g <zzj@tutanota.com>
2023-11-03 13:56:15 -0400

    add image display logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,30 @@ const Home: React.FC = () => {
   const [maleAllele, setMaleAllele] = useState<string>("E"); // Default to first allele of 'E'
   const [results, setResults] = useState<string>("");
 
+  //type definition for alleleImageMap
+  type AlleleImageMap = {
+    [key: string]: {
+      [allele: string]: string;
+    };
+  };
+  //mapping for all the male chicken pics
+  const maleAlleleImageMap: AlleleImageMap = {
+    E: {
+      "e+/e+": "/images/male/white_rooster.png",
+    },
+  };
+  //mapping for all the female chicken pics
+  const femaleAlleleImageMap: AlleleImageMap = {
+    E: {
+      "e+/e+": "/images/female/black_unicolor.png",
+    },
+  };
+
+  // state managment for images
+  const [maleImage, setMaleImage] = useState("");
+  const [femaleImage, setFemaleImage] = useState("");
+  const [resultImage, setResultImage] = useState("");
+
   useEffect(() => {
     const fetchGeneData = async () => {
       try {
@@ -43,6 +67,21 @@ const Home: React.FC = () => {
     ];
     return `Possible Alleles Combinations: ${possibleCombinations.join(", ")}`;
   };
+
+  const updateMaleImage = (gene: string, allele: string) => {
+    const imagePath = maleAlleleImageMap[gene]?.[allele];
+    if (imagePath) {
+      setMaleImage(imagePath);
+    }
+  };
+
+  const updateFemaleImage = (gene: string, allele: string) => {
+    const imagePath = femaleAlleleImageMap[gene]?.[allele];
+    if (imagePath) {
+      setFemaleImage(imagePath);
+    }
+  };
+
   const calculateResults = () => {
     const featherColor = calculateAlleles(
       selectedGene,
@@ -50,6 +89,7 @@ const Home: React.FC = () => {
       maleAllele
     );
     setResults(`Potential Feather Color: ${featherColor}`);
+    setResultImage("images/output/black_unicolor_elf.png");
   };
 
   return (
@@ -89,7 +129,11 @@ const Home: React.FC = () => {
         <select
           id="maleAllele"
           value={maleAllele}
-          onChange={(e) => setMaleAllele(e.target.value)}
+          onChange={(e) => {
+            const newAllele = e.target.value;
+            setMaleAllele(newAllele);
+            updateMaleImage(maleGene, newAllele);
+          }}
           className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         >
           {geneData[maleGene]?.alleles.map((allele: string) => (
@@ -99,6 +143,7 @@ const Home: React.FC = () => {
           ))}
         </select>
       </div>
+      <img src={maleImage} alt="Male Chicken" width="200" height="200" />
       <div className="mb-4">
         <label
           htmlFor="femaleGene"
@@ -132,7 +177,11 @@ const Home: React.FC = () => {
         <select
           id="femaleAllele"
           value={femaleAllele}
-          onChange={(e) => setFemaleAllele(e.target.value)}
+          onChange={(e) => {
+            const newAllele = e.target.value;
+            setFemaleAllele(newAllele);
+            updateFemaleImage(femaleGene, newAllele);
+          }}
           className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         >
           {geneData[femaleGene]?.alleles.map((allele: string) => (
@@ -142,7 +191,7 @@ const Home: React.FC = () => {
           ))}
         </select>
       </div>
-
+      <img src={femaleImage} alt="Female Chicken" width="200" height="200" />
       <button onClick={calculateResults} className="p-2 border rounded mt-4">
         Calculate
       </button>
